925ef1ea17a5fed5f5c5185c92032f1192d987bd
ranzongqian <18883180383@163.com>
2019-04-23 10:27:34 +0800

    3

--- myvscode/test/demo/src/components/FAQ.vue
+++ myvscode/test/demo/src/components/FAQ.vue
@@ -1,11 +1,21 @@
 <template>
     <main class="faq">
         <h1>Frenquently Asked Questions</h1>
+
+        <div class="error" v-if="error">
+            can't load the questions
+        </div>
     </main>
 </template>
 
 <script>
 export default {
-  name: 'faq'
+  name: 'faq',
+  data () {
+    return {
+      questions: [],
+      error: null
+    }
+  }
 }
 </script>
